fa3537a24a8c9e7b4d47e6e8f409618ce893d1d5
Sadaqat Ali <sadaqatali890@gmail.com>
2024-11-08 20:53:51 +0530

    modify reset email blade

--- resources/views/emails/reset-email.blade.php
+++ resources/views/emails/reset-email.blade.php
@@ -1,4 +1,4 @@
 <p>Hello, {{ $name }}</p>
 <p>You requested a password reset. Click the link below to reset your password:</p>
-<a href="{{ $resetLink }}">{{ $resetLink }}</a>
+<a href="{{ $resetLink }}">Please Click Here</a>
 <p>If you did not request this, please ignore this email.</p>
