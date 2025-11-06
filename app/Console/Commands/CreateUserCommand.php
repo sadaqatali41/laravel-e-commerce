817c4e97f54fcafcfac17c9aa93c7164c8cd1105
Sadaqat Ali <sadaqatali890@gmail.com>
2025-11-06 20:51:18 +0530

    Add create command to create user and admin with CLI

--- app/Console/Commands/CreateUserCommand.php
+++ app/Console/Commands/CreateUserCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\User;
 use App\Models\Admin\Admin;
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -36,38 +38,58 @@ class CreateUserCommand extends Command
             0
         );
 
-        if($userType === 'Admin') {
-            $name = $this->ask('Enter your name.');
-            $email = $this->ask('Enter your email.');
-            $password = $this->secret('Enter your password.');
+        // common inputs for both Admin & users
+        $commonData['name'] = $this->ask('Enter your name.');
+        $commonData['email'] = $this->ask('Enter your email.');
+        $commonData['password'] = $this->secret('Enter your password.');
 
-            $validator = Validator::make([
-                'name'      => $name,
-                'email'     => $email,
-                'password'  => $password
-            ], [
-                'name' => ['required', 'min:3', 'max:20'],
-                'email' => ['required', 'email', 'unique:admins,email'],
-                'password'  => ['required', 'min:6', 'max:16']
-            ]);
-
-            if($validator->fails()) {
-                $this->info($userType . ' is not created. See error messages below:');
-
-                foreach ($validator->errors()->all() as $error) {
-                    $this->error($error);
-                }
-                return 1;
-            }
-            
-            Admin::create([
-                'name' => $name,
-                'email' => $email,
-                'password' => Hash::make($password)
-            ]);
-
-            $this->info($userType . ' is created successfully. Now login with the same credentials.');
+        // admin specific data
+        if($userType !== 'Admin') {
+            $commonData['mobile'] = $this->ask('Enter Mobile Number.');
+            $commonData['activation_token'] = Str::random(60);
+            $commonData['ip'] = request()->ip();
+            $commonData['status'] = 'A';
+            $commonData['email_verified_at'] = now();
         }
+
+        // common validations
+        $commonValidation = [
+            'name' => ['required', 'min:3', 'max:20'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password'  => ['required', 'min:6', 'max:16'],
+            'mobile' => $userType === 'User' ? 'required|digits:10' : 'nullable'
+        ];
+
+        // admin specific validation
+        $adminValidation = [
+            'email' => 'required|email|unique:admins,email'         
+        ];
+
+        if($userType === 'Admin') {
+            $commonValidation = array_merge($commonValidation, $adminValidation);
+        }
+
+        // perform validations
+        $validator = Validator::make($commonData, $commonValidation);
+
+        if($validator->fails()) {
+            $this->info($userType . ' is not created. See error messages below:');
+
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+            return 1;
+        }
+
+        // hash the password
+        $commonData['password'] = Hash::make($commonData['password']);
+
+        if($userType === 'Admin') {
+            Admin::create($commonData);
+        } else {
+            User::create($commonData);
+        }
+        $this->info($userType . ' is created successfully. Now login with the same credentials.');
         return 0;
     }
 }
